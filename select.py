5a8b0f772091e9fc65b2117108971157c9d89daf
jun, li <42463488+Neooolee@users.noreply.github.com>
2022-09-01 09:16:48 +0800

    Update select.py

--- select.py
+++ select.py
@@ -11,24 +11,40 @@ from gdaldiy import *
 import openpyxl
 """
  the file should be organized as:
- ./train/cloud/10mclips/imagename/patch_Id.tif
- ./train/cloud/10mclips/imagename/patch_Id.tif
- ./test/cloud/10mclips/imagename/patch_Id.tif
- ./test/cloud/10mclips/imagename/patch_Id.tif
+ ./train
+        /cloud
+             /10mclips/imagename/patch_Id.tif
+             /20mclips/imagename/patch_Id.tif
+             /60mclips/imagename/patch_Id.tif
+  ./train
+        /clear
+             /10mclips/imagename/patch_Id.tif
+             /20mclips/imagename/patch_Id.tif
+             /60mclips/imagename/patch_Id.tif
+ ./test
+        /cloud
+             /10mclips/imagename/patch_Id.tif
+             /20mclips/imagename/patch_Id.tif
+             /60mclips/imagename/patch_Id.tif
+ ./test
+        /clear
+             /10mclips/imagename/patch_Id.tif
+             /20mclips/imagename/patch_Id.tif
+             /60mclips/imagename/patch_Id.tif
 """
-dirs=['./cloud/','./clear/']#filedir
-targetnames=['10mclips','20mclips']
+commendir='F:/WHU/WHUS2-CR/composite/'#source dir
+filedirs=["cloud/","clear/"]
+targetnames=['10mclips','20mclips','60mclips']
 filetypes=['train','test']
   
 def selectfromxls(xlpath,filedir,targetname):
     wb=openpyxl.load_workbook(xlpath)
     for filetype in filetypes:
         ws=wb[filetype]#fileno for train data.
-        # filedirs=glob.glob(os.path.join(filedir+targetname, '*'))
         for i in range(1,ws.max_row+1):
             name = str(ws.cell(row=i,column=1).value)
-            pairpath=filedir+targetname+name
-            savedir = filedir[:-6]+filetype+'/'+filedir[-6:]+targetname[:-5]+'/'+name.split('/')[1]
+            pairpath=commendir+filedir+targetname+name
+            savedir = commendir+"selected/"+filedir+targetname+'/'+name.split('/')[1]
             if not os.path.exists(pairpath): 
                 continue
             if not os.path.exists(savedir): 
@@ -37,7 +53,7 @@ def selectfromxls(xlpath,filedir,targetname):
             if not os.path.exists(savepath):
                 selectedimg=imgread(pairpath)#read file function. You can replace this function for reading images.
                 imgwrite(savepath,selectedimg)##save file function.You can replace this function for saving images.
-for filedir in dirs:
+for filedir in filedirs:
     for targetname in targetnames:
         selectfromxls("./filenos.xlsx",filedir,targetname)
 
